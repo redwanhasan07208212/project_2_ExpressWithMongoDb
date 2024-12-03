e506f776ade8fd51586a0bba6d478559cda9f295
Redwan <76751002+redwanhasan07208212@users.noreply.github.com>
2024-12-03 15:02:20 +0600

    repeating code removed by catchAsync function and also use requestHandler

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,33 +1,28 @@
-import { NextFunction, Request, Response } from 'express';
+import { NextFunction, Request, RequestHandler, Response } from 'express';
 import httpStatus from 'http-status';
 import sendResponse from '../../utils/sendResponse';
 import { userServices } from './user.service';
 
-const createStudent = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const { password, student: studentData } = req.body;
-    // console.log(password,studentData);
-
-    // const zodParsedData = studentValidationSchema.parse(studentData);
-
-    const result = await userServices.createStudentIntoDb(
-      password,
-      studentData,
-    );
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Student is created successfully',
-      data: result,
-    });
-  } catch (err) {
-    next(err);
-  }
+const catchAsync = (fn: RequestHandler) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch((err) => next(err));
+  };
 };
+
+const createStudent = catchAsync(async (req, res, next) => {
+  const { password, student: studentData } = req.body;
+  // console.log(password,studentData);
+
+  // const zodParsedData = studentValidationSchema.parse(studentData);
+
+  const result = await userServices.createStudentIntoDb(password, studentData);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student is created successfully',
+    data: result,
+  });
+});
 
 export const UserControllers = {
   createStudent,
