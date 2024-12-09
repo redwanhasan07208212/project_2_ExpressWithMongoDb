b8a6dd3269cdd24e6278ddeb2d2145b628757774
Redwan <76751002+redwanhasan07208212@users.noreply.github.com>
2024-12-09 17:46:30 +0600

    created academic Department controller successfully

--- src/app/modules/academicDepartment/academicDepartment.controller.ts
+++ src/app/modules/academicDepartment/academicDepartment.controller.ts
@@ -1,1 +1,62 @@
-const 
+import httpStatus from 'http-status';
+import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
+import { AcademicDepartmentService } from './academicDepartment.service';
+
+const createAcademicDepartment = catchAsync(async (req, res, next) => {
+  const result = await AcademicDepartmentService.createAcademicDepartintoDb(
+    req.body,
+  );
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Department created Successfully',
+    data: result,
+  });
+});
+
+const getAllAcademicDepartment = catchAsync(async (req, res, next) => {
+  const result =
+    await AcademicDepartmentService.getAllAcademicDepartmentintoDb();
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Get all Academic Department Successfully',
+    data: result,
+  });
+});
+
+const getSingleAcademicDepartment = catchAsync(async (req, res, next) => {
+  const { departmentId } = req.params;
+  const result =
+    await AcademicDepartmentService.getSingleAcademicDepartmentintoDb(
+      departmentId,
+    );
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Get Single Academic Department Successfully',
+    data: result,
+  });
+});
+
+const updateAcademicDepartment = catchAsync(async (req, res, next) => {
+  const { departmentId } = req.params;
+  const result = await AcademicDepartmentService.updateAcademicDepartmentintoDb(
+    departmentId,
+    req.body,
+  );
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Get Single Academic Department Successfully',
+    data: result,
+  });
+});
+
+export const AcademicDepartmentController = {
+  createAcademicDepartment,
+  getAllAcademicDepartment,
+  getSingleAcademicDepartment,
+  updateAcademicDepartment,
+};
