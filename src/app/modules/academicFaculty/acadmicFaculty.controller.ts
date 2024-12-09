0341abdadb1ac9f2ba2b6b72ab139c4d33c02f9f
Redwan <76751002+redwanhasan07208212@users.noreply.github.com>
2024-12-09 13:51:57 +0600

    academic faculty controller logic done

---
+++ src/app/modules/academicFaculty/acadmicFaculty.controller.ts
@@ -0,0 +1,55 @@
+import httpStatus from 'http-status';
+import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
+import { AcademicFacultyServices } from './academicFaculty.service';
+
+const createacademicFaculty = catchAsync(async (req, res, next) => {
+  const result = await AcademicFacultyServices.createAcademicFacultyintoDb(
+    req.body,
+  );
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Faculty Created Successfully',
+    data: result,
+  });
+});
+const getAllFromAcademicFaculty = catchAsync(async (req, res, next) => {
+  const result = await AcademicFacultyServices.getAllAcademicFacultyintoDb();
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Faculty Created Successfully',
+    data: result,
+  });
+});
+const getSingleFromAcademicFaculty = catchAsync(async (req, res, next) => {
+  const { facultyId } = req.params;
+  const result =
+    await AcademicFacultyServices.getSingleAcademicFacultyintoDb(facultyId);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Faculty Created Successfully',
+    data: result,
+  });
+});
+const updateAcademicFaculty = catchAsync(async (req, res, next) => {
+  const { facultyId } = req.params;
+  const result = await AcademicFacultyServices.updateAcademicFacultyintoDb(
+    facultyId,
+    req.body,
+  );
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Faculty Created Successfully',
+    data: result,
+  });
+});
+export const AcademicFacultyController = {
+  createacademicFaculty,
+  getAllFromAcademicFaculty,
+  getSingleFromAcademicFaculty,
+  updateAcademicFaculty,
+};
