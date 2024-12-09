fc0c9cb9ef3b0f16dc9ba121ed91636f50db1dcc
Redwan <76751002+redwanhasan07208212@users.noreply.github.com>
2024-12-09 11:46:06 +0600

    fix the bug of id

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -16,10 +16,23 @@ const findStudentLastId = async () => {
     })
     .lean();
 
-  return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
+  return lastStudent?.id ? lastStudent.id : undefined;
 };
 export const generateStudentId = async (payload: TAcademicSemester) => {
-  const currentId = (await findStudentLastId()) || (0).toString();
+  let currentId = (0).toString();
+  const lastStudentId = await findStudentLastId();
+  //2024 02 0001
+  const lastStudentYear = lastStudentId?.substring(0, 4);
+  const lastStudentCode = lastStudentId?.substring(4, 6);
+  const currentStudentYear = payload.year;
+  const currentStudentCode = payload.code;
+  if (
+    lastStudentId &&
+    lastStudentYear === currentStudentYear &&
+    lastStudentCode === currentStudentCode
+  ) {
+    currentId = lastStudentId.substring(6);
+  }
   let incrementId = (parseInt(currentId) + 1).toString().padStart(4, '0');
   console.log(incrementId);
 
