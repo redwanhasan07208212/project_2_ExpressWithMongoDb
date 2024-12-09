d06ec3cb1dbd6c1dc5155ed8b9d90f9653e4b838
Redwan <76751002+redwanhasan07208212@users.noreply.github.com>
2024-12-09 11:49:01 +0600

    update the academic semester model file

--- src/app/modules/academicSemester/academicSemester.model.ts
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -2,32 +2,37 @@ import { model, Schema } from 'mongoose';
 import { codeName, Months, semesterName } from './academicSemester.constant';
 import { TAcademicSemester } from './academicSemester.interface';
 
-const AcademicSemeterModel = new Schema<TAcademicSemester>({
-  name: {
-    type: String,
-    enum: semesterName,
-    required: true,
+const AcademicSemeterModel = new Schema<TAcademicSemester>(
+  {
+    name: {
+      type: String,
+      enum: semesterName,
+      required: true,
+    },
+    code: {
+      type: String,
+      enum: codeName,
+      required: true,
+    },
+    year: {
+      type: String,
+      required: true,
+    },
+    startMonth: {
+      type: String,
+      enum: Months,
+      required: true,
+    },
+    endMonth: {
+      type: String,
+      enum: Months,
+      required: true,
+    },
   },
-  code: {
-    type: String,
-    enum: codeName,
-    required: true,
+  {
+    timestamps: true,
   },
-  year: {
-    type: String,
-    required: true,
-  },
-  startMonth: {
-    type: String,
-    enum: Months,
-    required: true,
-  },
-  endMonth: {
-    type: String,
-    enum: Months,
-    required: true,
-  },
-});
+);
 AcademicSemeterModel.pre('save', async function (next) {
   const isSemesterExits = await AcademicSemester.findOne({
     year: this.year,
