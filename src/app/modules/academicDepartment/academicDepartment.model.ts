42c9c9c15bf28e267b611177b8b245ac8caf8aaa
Redwan <76751002+redwanhasan07208212@users.noreply.github.com>
2024-12-09 18:11:14 +0600

    timestampts added in the academic department model

--- src/app/modules/academicDepartment/academicDepartment.model.ts
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -1,19 +1,24 @@
 import { model, Schema } from 'mongoose';
 import { TAcademicDepartment } from './academicDepartment.interface';
 
-const academicDepartmentSchema = new Schema<TAcademicDepartment>({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
+const academicDepartmentSchema = new Schema<TAcademicDepartment>(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    academicFaculty: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      unique: true,
+      ref: 'AcademicFaculty',
+    },
   },
-  academicFaculty: {
-    type: Schema.Types.ObjectId,
-    required: true,
-    unique: true,
-    ref: 'AcademicFaculty',
+  {
+    timestamps: true,
   },
-});
+);
 
 export const AcademicDepartment = model<TAcademicDepartment>(
   'AcademicDepartment',
